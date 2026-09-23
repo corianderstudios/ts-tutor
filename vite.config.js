9d55c5071040f6bc7d35f7ff7dde70b78d600b65
akernizan <akernizan@gmail.com>
2026-09-23 14:42:10 -0400

    updatdes for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/ts-tutor/",
   test: {
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.js',
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.js",
     globals: true,
   },
 });
